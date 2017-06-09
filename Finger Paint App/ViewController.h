5a2eda45b6584bd4490ffe1849a380986b9b8ed4
kevin Cleathero <kengc@hotmail.com>
2017-06-09 12:47:41 -0700

    project setup

---
+++ Finger Paint App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Finger Paint App
+//
+//  Created by Kevin Cleathero on 2017-06-09.
+//  Copyright © 2017 Kevin Cleathero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
